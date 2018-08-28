04c4b87834dbc3f8e6ad7cdbaad2edf00624abab
Jason Nelson <jason@carbonmade.com>
2018-08-28 16:54:28 -0700

    Add test coverage for ErrorResult

---
+++ src/Constellix.Tests/Models/ErrorResultTests.cs
@@ -0,0 +1,19 @@
+﻿using Carbon.Json;
+using Xunit;
+
+namespace Constellix.Dns.Tests
+{
+    public class ErrorResultTests
+    {
+        [Fact]
+        public void Parse()
+        {
+            var text = @"{""errors"":[""Record not found""]}";
+
+            var result = JsonObject.Parse(text).As<ErrorResult>();
+
+            Assert.Equal("Record not found", result.Errors[0]);
+
+        }
+    }
+}
