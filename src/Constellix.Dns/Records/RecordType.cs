ca0c5dae31c18778243b26171162e1932c3f3623
Jason Nelson <jason@carbonmade.com>
2018-08-28 16:54:43 -0700

    Set RecordType values

--- src/Constellix.Dns/Records/RecordType.cs
+++ src/Constellix.Dns/Records/RecordType.cs
@@ -2,21 +2,21 @@
 {
     public enum RecordType
     {
-        A,
-        AAAA,
-        ANAME,
-        CNAME,
-        CINFO,
-        MX,
-        NAPTR,
-        CAA,
-        CERT,
-        NS,
-        PTR,
-        RP,
-        SPF,
-        SRV,
-        TXT,
-        HTTPRedirection
+        A               = 1,
+        AAAA            = 2,
+        ANAME           = 3,
+        CNAME           = 4,
+        CINFO           = 5,
+        MX              = 6,
+        NAPTR           = 7,
+        CAA             = 8,
+        CERT            = 9,
+        NS              = 10,
+        PTR             = 11,
+        RP              = 12,
+        SPF             = 13,
+        SRV             = 14,
+        TXT             = 15,
+        HTTPRedirection = 16
     }
 }
