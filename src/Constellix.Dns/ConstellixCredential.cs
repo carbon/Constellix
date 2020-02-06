b85843919992fac2bc80adf6e037794933ab6306
Jason Nelson <jason@carbonmade.com>
2020-02-05 22:43:57 -0600

    Add IConstellixCredential

---
+++ src/Constellix.Dns/ConstellixCredential.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constellix.Dns
+{
+    public sealed class ConstellixCredential : IConstellixCredential
+    {
+        public ConstellixCredential(string apiKey, string secretKey)
+        {
+            ApiKey = apiKey ?? throw new ArgumentNullException(nameof(apiKey));
+            SecretKey = Encoding.UTF8.GetBytes(secretKey);
+        }
+
+        public bool ShouldRenew => false;
+
+        public ValueTask RenewAsync()
+        {
+            return new ValueTask();
+        }
+
+        public string ApiKey { get; }
+
+        public byte[] SecretKey { get; }
+    }
+}
+
+// ref: https://api-dns-docs.constellix.com/
---
+++ src/Constellix.Dns/IConstellixCredential.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace Constellix.Dns
+{
+    public interface IConstellixCredential
+    {
+        bool ShouldRenew { get; }
+
+        ValueTask RenewAsync();
+
+        string ApiKey { get; }
+
+        // plaintext as UTF8
+        byte[] SecretKey { get; }
+    }
+}
+
+// ref: https://api-dns-docs.constellix.com/
