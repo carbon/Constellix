8da745042688478fe1cb47821c593770cce7053b
Jason Nelson <jason@carbonmade.com>
2020-12-23 17:26:28 -0600

    Add VanityNameServerDetails

---
+++ src/Constellix.Dns/Models/VanityNameServerDetails.cs
@@ -0,0 +1,24 @@
+﻿#nullable disable
+
+namespace Constellix.Dns
+{
+    public sealed class VanityNameServerDetails
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsDefault { get; set; }
+
+        public bool IsPublic { get; set; }
+
+        public string[] NameServers { get; set; }
+
+        public long NameserverGroup { get; set; }
+
+        public string NameserverGroupName { get; set; }
+
+        public string NameserversListString { get; set; }
+    }
+}
+ 
